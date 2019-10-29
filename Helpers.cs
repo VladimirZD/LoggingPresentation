12a6841c6e67611c277c67732edb0a459bc2981b
Vladimir Mašala <vladimir.masala@gmail.com>
2019-10-29 23:38:56 +0100

    Initial commit

---
+++ Helpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggingTipsTricks
+{
+    
+    class Helpers
+    {
+        public static NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using LoggingTipsTricks;
+using System;
+
+namespace dotnetcore_nlog
+{
+    class Program
+    {
+        
+
+        private string Folder { get; set; }
+        public string File{ get; set; }
+        public bool Convert { get; set; }
+        static void Main(string[] args)
+        {
+            
+            Helpers.Logger.Trace("Loggin trace.");
+
+        }
+
+        private string[] GetRDLFiles()
+        {
+            return null;
+        }
+      
+    }
+}
